540e9330e7c126e84c23be9c777213c22ef6cf08
viacheslav <kabaniets1990@gmail.com>
2025-02-26 17:34:24 +0200

    finished meals-project

--- Meals Project/src/components/Meals.tsx
+++ Meals Project/src/components/Meals.tsx
@@ -22,20 +22,28 @@ const Meals = () => {
         .includes(searchQuery.toLowerCase()));
 
     return (
-        <div className='min-h-screen flex flex-col items-center justify-center'>
-            <h1>Seafood Recipes</h1>
+        <div className='min-h-screen flex flex-col items-center justify-center p-8 bg-gray-100'>
+            <h1 className='text-4xl font-bold mb-8 text-teal-600'>Seafood Recipes</h1>
             <input
                 type="text"
                 placeholder="Search for a meal..."
                 value={searchQuery}
                 onChange={(e) => setSearchQuery(e.target.value)}
+                className='w-[20rem] p-2 mb-4 rounded-lg border border-gray-300'
             />
             <div>
                 {filteredMeals.length > 0 ? (
                     filteredMeals.map((meal) => (
-                        <div key={meal.idMeal}>
+                        <div
+                            className='bg-white shadow-lg rounded-lg p-4 mb-4 flex flex-col items-center justify-center'
+                            key={meal.idMeal}
+                        >
                             <h2>{meal.strMeal}</h2>
-                            <img src={meal.strMealThumb} alt={meal.strMeal} />
+                            <img
+                                className='w-full h-64 object-cover rounded-t-lg mb-4'
+                                src={meal.strMealThumb}
+                                alt={meal.strMeal}
+                            />
                         </div>
                     ))
                 ) : (
