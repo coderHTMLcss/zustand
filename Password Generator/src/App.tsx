aa8893b8490890d833507e1bf070b42632b2e9d7
viacheslav <kabaniets1990@gmail.com>
2025-02-26 13:09:24 +0200

    start password-generator-app

---
+++ Password Generator/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-amber-950 font-bold text-4xl'>
+      Password Generator
+    </div>
+  )
+}
+
+export default App
